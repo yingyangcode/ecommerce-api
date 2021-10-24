4004624c534359e60d8a708f6e83789b991175ab
yingyangcode <marcel.anis.21@gmail.com>
2021-10-24 02:09:57 -0400

    Implement CRUD operation for User model

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
+const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
-const productRoute = require("./routes/product");
-const userRoute = require("./routes/user");
+// const stripeRoute = require("./routes/stripe");
+// const cors = require("cors");
 
 dotenv.config();
 
@@ -18,12 +19,14 @@ mongoose
     console.log(err);
   });
 
+// app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
-app.use("/api/cart", cartRoute);
-app.use("/api/order", orderRoute);
-app.use("/api/product", productRoute);
-app.use("/api/user", userRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+// app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
