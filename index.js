1cf608479a98aa8f8449e4f7a2d557406f2bc3ce
yingyangcode <marcel.anis.21@gmail.com>
2021-10-23 21:58:48 -0400

    Implement /register and /login routes for authentication
    - User can register with email, password and username
    - Password is encrypted using crypto-js AES algorithm
    - When the user logs in, JWT access token is created and returned using id and isAdmin fields.

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
+const authRoute = require("./routes/auth");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const productRoute = require("./routes/product");
 const userRoute = require("./routes/user");
 
 dotenv.config();
@@ -14,6 +19,10 @@ mongoose
   });
 
 app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/order", orderRoute);
+app.use("/api/product", productRoute);
 app.use("/api/user", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
